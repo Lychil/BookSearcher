97423b7882cbe4b2e8d18a84053a104e927de16c
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2025-07-24 01:30:43 +0300

    home. Добавлен компонент поисковой строки

---
+++ src/modules/user/Home/Home.tsx
@@ -0,0 +1,10 @@
+import { SearchBar } from "@/common/components/SearchBar/SearchBar";
+import styles from "@/modules/user/Home/Home.module.css";
+
+export default function Home() {
+    return (
+        <div className={styles.wrapper}>
+            <SearchBar />
+        </div>
+    )
+}
--- src/router/RoutesProvider.tsx
+++ src/router/RoutesProvider.tsx
@@ -8,17 +8,20 @@ import {
 import ProtectedRoutes from "@/router/ProtectedRoutes";
 import Layout from "@/common/components/Layout/Layout";
 import AuthSwitcher from "@/modules/auth/AuthSwitcher";
+import Home from "@/modules/user/Home/Home";
 
 export default function RoutesProvider() {
-    const isAuth = false;
+    const isAuth = true;
 
     const router = createBrowserRouter(
         createRoutesFromElements(
             <>
                 <Route path="/" element={<Layout />}>
-                    <Route index element={<Navigate to="/auth" replace />} />
+                    <Route index element={<Navigate to={isAuth ? "/home" : "/login"} replace />} />
                     <Route path="auth" element={<AuthSwitcher />} />
-                    <Route element={<ProtectedRoutes isAllowed={isAuth} />}></Route>
+                    <Route element={<ProtectedRoutes isAllowed={isAuth} />}>
+                        <Route path="home" element={<Home />} />
+                    </Route>
                 </Route>
             </>
         )
