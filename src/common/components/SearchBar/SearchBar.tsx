86462418182ddea84de07acff76a25cafe0a70c6
iaroslav_shavrov <iaroslav_shavrov@practice.relex.ru>
2025-07-26 01:16:32 +0300

    home. Синхронизация поискового запроса с параметрами URL

--- src/common/components/SearchBar/SearchBar.tsx
+++ src/common/components/SearchBar/SearchBar.tsx
@@ -3,18 +3,28 @@ import styles from './SearchBar.module.css';
 import { FiSearch } from 'react-icons/fi';
 import { SearchFormData } from '@/common/types/filters';
 import { filtersValidations } from '@/common/validations/filters';
+import { useNavigate } from 'react-router-dom';
 
 export const SearchBar = () => {
-    const { register, handleSubmit, formState: { isSubmitting }} = useForm<SearchFormData>();
+    const { register, handleSubmit, formState: { isSubmitting } } = useForm<SearchFormData>();
+    const navigate = useNavigate();
 
-    const onSubmit = (data: SearchFormData) => {};
+    const onSubmit = (data: SearchFormData) => {
+        const query = data.query?.trim();
+
+        navigate({
+            pathname: window.location.pathname,
+            search: query ? `?query=${encodeURIComponent(query)}` : '',
+        });
+    };
+
 
     return (
         <form onSubmit={handleSubmit(onSubmit)} className={styles.searchForm}>
             <div className={styles.searchContainer}>
                 <input
                     type="text"
-                    placeholder="Поиск..."
+                    placeholder="Введите поисковой запрос..."
                     className={styles.searchInput}
                     {...register('query', filtersValidations.search)}
                 />
--- src/common/validations/filters.ts
+++ src/common/validations/filters.ts
@@ -1,9 +1,5 @@
 export const filtersValidations = {
     search: {
-        required: 'Введите поисковый запрос',
-        minLength: {
-            value: 2,
-            message: 'Минимум 2 символа'
-        }
+        required: false,
     }
 }
